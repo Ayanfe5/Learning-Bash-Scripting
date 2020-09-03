f211ecae4dfb9c8cdbb098d3923039362909c1e5
Ayanfe5 <adeifeib@gmail.com>
2020-09-03 11:24:08 +0100

    Module 2

--- script.sh
+++ script.sh
@@ -1,2 +1,25 @@
 echo Hello World
 
+echo This is Adeola present working directory $PWD
+
+echo This is my bash $BASH
+
+echo This is my home $HOME
+
+echo This is my bash version $BASH_VERSION 
+
+name='my name is Ayanfeoluwa Adeola'
+
+echo This is my name $name
+
+school='I attended University Of Ilorin,Kwara State'
+ 
+echo This is my school $school
+
+course='Where I studied Chemistry'
+
+echo This is my course $course
+
+founded='Unilorin was founded in the year 1975'
+
+echo This is my founded $founded
